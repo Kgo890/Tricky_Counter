972c7c95d4c56f9a688c6f02aa1f620fbfb667cf
Kgo890 <gorekaden501@gmail.com>
2025-02-02 21:26:45 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+counter = 0
+for items in my_list:
+    counter = counter + items
+print(counter)
+
+for items in my_list:
+    math = items * (items + 1)/2
+print(math)
